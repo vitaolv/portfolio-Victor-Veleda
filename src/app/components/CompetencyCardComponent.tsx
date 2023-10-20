18c385f57f9630fe81099c3b3b2d9105a7830276
vitaolv <vlveleda@gmail.com>
2023-10-20 02:56:58 -0300

    Correção de responsividade

--- src/app/components/CompetencyCardComponent.tsx
+++ src/app/components/CompetencyCardComponent.tsx
@@ -6,30 +6,25 @@ export function CompetencyCardComponent() {
         <>
             {MyCompetencies.map((category) => (
                 <div key={category.id} className="max-w-md mx-auto p-4">
-                    <p className="font-alt text-xl mt-6 mb-3 md:h-16 w-auto sm:w-96 md:w-96 h-auto text-center md:text-left">
+                    <p className="font-alt text-xl mt-6 mb-3 md:h-16 w-auto sm:w-96 md:w-96 h-auto text-center">
                         {category.title}
                     </p>
                     <div className="border-green-700 border-2 shadow-gradient-green-blue shadow-green-700 p-10 rounded-lg md:w-96 w-auto text-center md:text-left">
-                        <ul>
-                            <li className="grid grid-cols-1 md:grid-rows-2 gap-4 justify-center">
-                                {category.items.map((item) => (
-                                    <div
-                                        key={item.id}
-                                        className="flex flex-row w-auto md:w-56 mx-auto items-center gap-4"
-                                    >
-                                        <Image
-                                            className="m-0 h-24"
-                                            alt="icon"
-                                            width={58}
-                                            height={58}
-                                            src={item.icon}
-                                        />
-                                        <p className="font-sans text-left text-md md:text-lg font-medium items-center w-32 sm:w-56 md:w-56">
-                                            {item.name}
-                                        </p>
-                                    </div>
-                                ))}
-                            </li>
+                        <ul className="flex flex-wrap justify-center gap-4">
+                            {category.items.map((item) => (
+                                <div key={item.id} className="flex flex-row w-auto md:w-56 mx-auto items-center gap-4">
+                                    <Image
+                                        className="m-0 h-24"
+                                        alt="icon"
+                                        width={55}
+                                        height={55}
+                                        src={item.icon}
+                                    />
+                                    <p className="font-sans text-left text-md md:text-lg font-medium items-center w-32 sm:w-56 md:w-56">
+                                        {item.name}
+                                    </p>
+                                </div>
+                            ))}
                         </ul>
                     </div>
                 </div>
