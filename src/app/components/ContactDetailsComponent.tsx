e0624d645928ea64cf1070ad3cffc032539b00e2
vitaolv <vlveleda@gmail.com>
2023-10-31 16:13:53 -0300

    Adicionando Link de NextJS na seção "Contato".

--- src/app/components/ContactDetailsComponent.tsx
+++ src/app/components/ContactDetailsComponent.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image"
 import { Contacts } from "@/app/utils/dates/Contacts";
 
+import Link from "next/link";
+
 export function ContactDetailsComponent() {
 
     return (
@@ -10,18 +12,20 @@ export function ContactDetailsComponent() {
                 {Contacts.map((items) => (
                     <ul key={items.id} className="mx-auto">
                         <li>
-                            <Image
-                                className="´p-0 m-0 mx-auto rounded-xl"
-                                src={items.icon}
-                                width={50}
-                                height={50}
-                                alt={items.name}
-                                data-noindex="true"
-                            />
+                            <Link href={items.link}>
+                                <Image
+                                    className="´p-0 m-0 mx-auto rounded-xl"
+                                    src={items.icon}
+                                    width={50}
+                                    height={50}
+                                    alt={items.name}
+                                    data-noindex="true"
+                                />
+                            </Link>
                         </li>
                     </ul>
                 ))}
-            </div>
+            </div >
 
             <p className="w-auto mt-6 mb-3 text-lg font-alt text-center">
                 Ou, entre em contato por via E-mail:
