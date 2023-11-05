73f2f826b4f321474372618a998c4353e167c697
vitaolv <vlveleda@gmail.com>
2023-11-05 16:15:38 -0300

    Quebrando de linha na string para facilitar a leitura dos usuários.

--- src/app/utils/dates/MyProjects.tsx
+++ src/app/utils/dates/MyProjects.tsx
@@ -15,10 +15,10 @@ export const projects: {
       description: `
       Participei de um desafio de desenvolvimento front-end para criar um E-commerce em uma única página, 
       utilizando HTML, CSS e Bootstrap, sem JavaScript. 
-      A ausência de recursos adicionais, como JavaScript, representou um grande desafio, 
+      \nA ausência de recursos adicionais, como JavaScript, representou um grande desafio, 
       especialmente ao criar um E-commerce com 6 seções em uma única página. A preocupação central 
       foi manter um código limpo e bem estruturado, respeitando as limitações técnicas impostas. 
-      Essa abordagem proporcionou uma valiosa experiência ao explorar a organização e a responsividade
+      \nEssa abordagem proporcionou uma valiosa experiência ao explorar a organização e a responsividade
       do design, destacando a importância de estratégias semânticas e técnicas de layout eficazes.
       Este desafio demonstrou a capacidade de trabalhar com restrições, enfatizando a necessidade 
       de uma abordagem cuidadosa e eficiente no desenvolvimento front-end, mesmo sem o uso de recursos avançados.`,
@@ -36,11 +36,11 @@ export const projects: {
       liveDemogithubLink: "https://vitaolv.github.io/project-vite-react-ecommerce/",
       description: `
       O projeto consiste em um amplo e detalhado site de e-commerce e um sofisticado carrinho de compras, 
-      oferecendo aos usuários uma gama de funcionalidades. Além das possibilidades de visualizar a 
+      oferecendo aos usuários uma gama de funcionalidades. \nAlém das possibilidades de visualizar a 
       página principal, pesquisar produtos, adicionar itens, selecionar quantidades e gerenciar 
       o carrinho (com opções para remover produtos e calcular o preço total das compras), 
       o projeto abrange uma arquitetura robusta, ações interativas, diversos estados, 
-      componentes variados e múltiplas páginas. Desenvolvido em React com JavaScript, 
+      componentes variados e múltiplas páginas. \nDesenvolvido em React com JavaScript, 
       o projeto segue a metodologia da arquitetura limpa, visando um código limpo e de fácil manutenção. 
       Utilizando o Redux para gerenciar o estado global da aplicação, a experiência de desenvolvimento 
       tem sido incrível, permitindo um constante aprimoramento do projeto, com o objetivo contínuo de 
@@ -65,7 +65,7 @@ export const projects: {
       liveDemogithubLink: "",
       description: `
       Em andamento, o projeto visa oferecer soluções personalizadas em saúde, incorporando abordagens 
-      inovadoras para o atendimento presencial e digital. A proposta abrange o 
+      inovadoras para o atendimento presencial e digital. \nA proposta abrange o 
       desenvolvimento de um website utilizando TypeScript com React e SASS, visando 
       fornecer uma plataforma moderna e adaptável às necessidades do setor de saúde. 
       Com a escolha dessas tecnologias, busca-se integrar a robustez do TypeScript com a flexibilidade 
