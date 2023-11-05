a7e111c811ab5b2596a59158cf79c40b30f6dc19
vitaolv <vlveleda@gmail.com>
2023-11-05 17:21:34 -0300

    Criando uma função para a quebra de linha na descrição de projeto, na seção "Projetos".

--- src/app/components/ProjectsList.tsx
+++ src/app/components/ProjectsList.tsx
@@ -4,56 +4,73 @@ import Image from "next/image";
 import Link from "next/link";
 
 export function ProjetctsList() {
+
     return (
         <div className="relative mt-12 m-6">
-            <ul className="grid grid-cols-1 md:grid-rows-2 gap-0">
-                {projects.map((project, index) => (
-                    <li
-                        className={`js-show-on-scroll transition-opacity invisible mt-6 flex flex-col justify-center md:justify-evenly ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse '}`}
-                        key={project.id}
-                    >
-                        <div className="flex flex-col items-center md:w-96 mx-auto md:mx-0">
-                            <div className="">
-                                <Image
-                                    className=" shadow-gradient-green-blue rounded-2xl border-1 border-blue-50 shadow-green-200"
-                                    src={project.gif}
-                                    width={480}
-                                    height={80}
-                                    alt="E-commerce.gif"
-                                />
-                            </div>
-                            <div className="flex flex-col text-justify">
-                                <h2 className="pb-5 pt-8 text-center font-alt text-lg text-green-700">{project.name}</h2>
-                                <h2 className="">{project.description}</h2>
-                            </div>
+            <div className="grid grid-cols-1 md:grid-rows-2 gap-0">
+                {projects.map((project, index) => {
 
-                            <div className="mb-0 flex flex-row gap-8 justify-between mt-8 md:-mb-24">
-                                <Link className="outline-none" href={project.githubLink}>
-                                    <div className="mx-auto outline-none font-alt flex flex-col items-center cursor-pointer gap-2">
+                    const projectDescription = project.description.split('\n').map((paragraph) =>
+                        <p className="mb-4" key={paragraph}>{paragraph}</p>
+                    );
+
+                    return (
+                        <ul key={project.id}>
+                            <li
+                                className={`js-show-on-scroll transition-opacity invisible mt-6 flex 
+                            flex-col justify-center md:justify-evenly ${index % 2 === 0 ? 'md:flex-row' : 'md:flex-row-reverse '}`}
+                            >
+                                <div className="flex flex-col items-center md:w-96 mx-auto md:mx-0">
+                                    <div className="">
                                         <Image
-                                            className="rounded-full shadow-gradient-green-blue shadow-green-200"
-                                            src="/portfolio-Victor-Veleda/assets/icons/code.svg"
-                                            width={40} height={40} alt="Ver código" />
-                                        <p className="font-alt text-md">Ver código</p>
+                                            className=" shadow-gradient-green-blue rounded-2xl 
+                                        border-1 border-blue-50 shadow-green-200"
+                                            src={project.gif}
+                                            width={480}
+                                            height={80}
+                                            alt="E-commerce.gif"
+                                        />
                                     </div>
-                                </Link>
-                                <Link className="outline-none" href={project.liveDemogithubLink}>
-                                    <div className="mx-auto outline-none flex flex-col items-center cursor-pointer gap-2">
-                                        <Image className="rounded-full shadow-gradient-green-blue shadow-green-200" src="/portfolio-Victor-Veleda/assets/icons/see.svg" width={40} height={40} alt="Visualizar" />
-                                        <p className="font-alt text-md">Visualizar</p>
+                                    <div className="flex flex-col text-justify">
+                                        <h2 className="pb-5 pt-8 text-center font-alt text-lg
+                                        text-green-700">{project.name}</h2>
+                                        <h2>{projectDescription}</h2>
                                     </div>
-                                </Link>
-                            </div>
-                        </div>
-                        <div
-                            className="h-[80px] w-[400px] 
-                                rounded-full bg-transparent bg-opacity-50 blur-[0px]"
-                            z-index="-1"
-                        />
-                    </li>
-                ))}
-            </ul>
+
+                                    <div className="mb-0 flex flex-row gap-8 justify-between mt-8 md:-mb-24">
+                                        <Link className="outline-none" href={project.githubLink}>
+                                            <div className="mx-auto outline-none font-alt flex flex-col 
+                                        items-center cursor-pointer gap-2">
+                                                <Image
+                                                    className="rounded-full shadow-gradient-green-blue shadow-green-200"
+                                                    src="/portfolio-Victor-Veleda/assets/icons/code.svg"
+                                                    width={40} height={40} alt="Ver código" />
+                                                <p className="font-alt text-md">Ver código</p>
+                                            </div>
+                                        </Link>
+                                        <Link className="outline-none" href={project.liveDemogithubLink}>
+                                            <div className="mx-auto outline-none flex flex-col items-center 
+                                        cursor-pointer gap-2">
+                                                <Image
+                                                    className="rounded-full shadow-gradient-green-blue 
+                                            shadow-green-200" src="/portfolio-Victor-Veleda/assets/icons/see.svg"
+                                                    width={40} height={40} alt="Visualizar" />
+                                                <p className="font-alt text-md">Visualizar</p>
+                                            </div>
+                                        </Link>
+                                    </div>
+                                </div>
+                                <div
+                                    className="h-[80px] w-[400px] 
+                                    rounded-full bg-transparent bg-opacity-50 blur-[0px]"
+                                    z-index="-1"
+                                />
+                            </li>
+                        </ul>
+                    );
+                })}
+            </div>
             <StripesProjectSection />
-        </div >
+        </div>
     )
 }
--- src/app/utils/dates/MyProjects.tsx
+++ src/app/utils/dates/MyProjects.tsx
@@ -13,15 +13,9 @@ export const projects: {
       githubLink: "https://github.com/vitaolv/projeto-frontend-web-HTML-CSS",
       liveDemogithubLink: "https://vitaolv.github.io/projeto-frontend-web-HTML-CSS/",
       description: `
-      Participei de um desafio de desenvolvimento front-end para criar um E-commerce em uma única página, 
-      utilizando HTML, CSS e Bootstrap, sem JavaScript. 
-      \nA ausência de recursos adicionais, como JavaScript, representou um grande desafio, 
-      especialmente ao criar um E-commerce com 6 seções em uma única página. A preocupação central 
-      foi manter um código limpo e bem estruturado, respeitando as limitações técnicas impostas. 
-      \nEssa abordagem proporcionou uma valiosa experiência ao explorar a organização e a responsividade
-      do design, destacando a importância de estratégias semânticas e técnicas de layout eficazes.
-      Este desafio demonstrou a capacidade de trabalhar com restrições, enfatizando a necessidade 
-      de uma abordagem cuidadosa e eficiente no desenvolvimento front-end, mesmo sem o uso de recursos avançados.`,
+      Participei de um desafio de desenvolvimento front-end para criar um E-commerce em uma única página, utilizando HTML, CSS e Bootstrap, sem JavaScript. 
+      A ausência de recursos adicionais, como JavaScript, representou um grande desafio, especialmente ao criar um E-commerce com 6 seções em uma única página. A preocupação central foi manter um código limpo e bem estruturado, respeitando as limitações técnicas impostas. 
+      Essa abordagem proporcionou uma valiosa experiência ao explorar a organização e a responsividade do design, destacando a importância de estratégias semânticas e técnicas de layout eficazes. Este desafio demonstrou a capacidade de trabalhar com restrições, enfatizando a necessidade de uma abordagem cuidadosa e eficiente no desenvolvimento front-end, mesmo sem o uso de recursos avançados.`,
       sources: [
         "HTML5",
         "CSS3",
@@ -35,16 +29,10 @@ export const projects: {
       githubLink: "https://github.com/vitaolv/project-vite-react-ecommerce",
       liveDemogithubLink: "https://vitaolv.github.io/project-vite-react-ecommerce/",
       description: `
-      O projeto consiste em um amplo e detalhado site de e-commerce e um sofisticado carrinho de compras, 
-      oferecendo aos usuários uma gama de funcionalidades. \nAlém das possibilidades de visualizar a 
-      página principal, pesquisar produtos, adicionar itens, selecionar quantidades e gerenciar 
-      o carrinho (com opções para remover produtos e calcular o preço total das compras), 
-      o projeto abrange uma arquitetura robusta, ações interativas, diversos estados, 
-      componentes variados e múltiplas páginas. \nDesenvolvido em React com JavaScript, 
-      o projeto segue a metodologia da arquitetura limpa, visando um código limpo e de fácil manutenção. 
-      Utilizando o Redux para gerenciar o estado global da aplicação, a experiência de desenvolvimento 
-      tem sido incrível, permitindo um constante aprimoramento do projeto, com o objetivo contínuo de 
-      aperfeiçoar a funcionalidade, a eficiência e a experiência do usuário.
+      O projeto consiste em um amplo e detalhado site de e-commerce e um sofisticado carrinho de compras, oferecendo aos usuários uma gama de funcionalidades. 
+      Além das possibilidades de visualizar a página principal, pesquisar produtos, adicionar itens, selecionar quantidades e gerenciar o carrinho (com opções para remover produtos e calcular o preço total das compras), o projeto abrange uma arquitetura robusta, ações interativas, diversos estados, componentes variados e múltiplas páginas. 
+      Desenvolvido em React com JavaScript, o projeto segue a metodologia da arquitetura limpa, visando um código limpo e de fácil manutenção. 
+      Utilizando o Redux para gerenciar o estado global da aplicação, a experiência de desenvolvimento tem sido incrível, permitindo um constante aprimoramento do projeto, com o objetivo contínuo de aperfeiçoar a funcionalidade, a eficiência e a experiência do usuário.
       `,
       sources: [
         "Javascript",
@@ -64,13 +52,9 @@ export const projects: {
       githubLink: "",
       liveDemogithubLink: "",
       description: `
-      Em andamento, o projeto visa oferecer soluções personalizadas em saúde, incorporando abordagens 
-      inovadoras para o atendimento presencial e digital. \nA proposta abrange o 
-      desenvolvimento de um website utilizando TypeScript com React e SASS, visando 
-      fornecer uma plataforma moderna e adaptável às necessidades do setor de saúde. 
-      Com a escolha dessas tecnologias, busca-se integrar a robustez do TypeScript com a flexibilidade 
-      e eficiência do React, juntamente com o poder de estilização do SASS, proporcionando um ambiente 
-      versátil e interativo para atender às demandas específicas desse setor em constante evolução.`,
+      Em andamento, o projeto visa oferecer soluções personalizadas em saúde, incorporando abordagens inovadoras para o atendimento presencial e digital. 
+      A proposta abrange o desenvolvimento de um website utilizando TypeScript com React e SASS, visando fornecer uma plataforma moderna e adaptável às necessidades do setor de saúde. 
+      Com a escolha dessas tecnologias, busca-se integrar a robustez do TypeScript com a flexibilidade e eficiência do React, juntamente com o poder de estilização do SASS, proporcionando um ambiente versátil e interativo para atender às demandas específicas desse setor em constante evolução.`,
       sources: [
         "TypeScript", "React", "SASS",
       ],
