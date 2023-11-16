9465338b0ae2b941466e8ec9cbafc746b4e15c2c
vitaolv <vlveleda@gmail.com>
2023-11-15 21:13:11 -0300

    Ajustando a margem.

--- src/app/sections/ContactSection.tsx
+++ src/app/sections/ContactSection.tsx
@@ -5,7 +5,7 @@ export function ContactSection() {
     return (
         <section id="contato"
             className="bg-[url(/portfolio-Victor-Veleda/assets/bg-stars.svg)] 
-            js-show-on-scroll transition-opacity invisible mt-28 mb-28">
+            js-show-on-scroll transition-opacity invisible mt-28 mb-32">
             <div className="mx-auto items-center text-center flex flex-col justify-center">
                 <h2 className="font-alt text-3xl md:text-6xl text-green-700 mb-12">Contato</h2>
 
