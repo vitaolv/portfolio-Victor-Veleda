358a7ac3408ffdfdf02aabaf8355772d8d0c4edd
vitaolv <vlveleda@gmail.com>
2023-12-07 03:27:10 -0300

    Criação do arquivo "ButtonScrollToTop.test.tsx" para testar o botão, responsável por direcionar ao topo da página.

---
+++ tests/components/ButtonScrollToTop.test.tsx
@@ -0,0 +1,22 @@
+import { screen, render } from "@testing-library/react";
+import { ButtonScrollToTop } from "@/app/components/ButtonScrollToTop";
+
+describe("This button componente is responsible for scrolling to the top when clicked", () => {
+
+    it.skip("should have div element", () => {
+        render(<ButtonScrollToTop />)
+
+    })
+
+    it.skip("should have an icon into the button element", () => {
+        render(<ButtonScrollToTop />)
+
+    })
+
+
+    it.skip("when the button is cliked, it should go to the top of the page", () => {
+        render(<ButtonScrollToTop />)
+
+    })
+
+});
