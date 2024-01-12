77f59d72628de1e31873244a133ab93d2bb3dea3
vitaolv <vlveleda@gmail.com>
2024-01-12 04:54:03 -0300

    Removendo o efeito opacity.

--- src/app/sections/CompetenciesSection.tsx
+++ src/app/sections/CompetenciesSection.tsx
@@ -9,7 +9,7 @@ export function CompetenciesSection() {
             className=" pt-28 md:pt-48 items-center text-center bg-[url(/portfolio-Victor-Veleda/assets/bg-stars.svg)] pb-4">
 
             <div className="">
-                <h2 className="font-alt text-3xl md:text-6xl text-green-700 js-show-on-scroll transition-opacity invisible" >Competências</h2>
+                <h2 className="font-alt text-3xl md:text-6xl text-green-700" >Competências</h2>
                 <div className="mt-2 mx-auto max-w-screen-xl flex flex-wrap justify-center gap-4 w-auto">
                     <CompetencyCardComponent />
                 </div>
