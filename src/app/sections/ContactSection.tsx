a5bb146999ded80687784146e45ce8f89dbb68a4
vitaolv <vlveleda@gmail.com>
2024-01-12 04:42:53 -0300

    Criação de tag div na seção Contato.

--- src/app/sections/ContactSection.tsx
+++ src/app/sections/ContactSection.tsx
@@ -6,30 +6,31 @@ export function ContactSection() {
         <section
             data-testid="contactSection-test"
             id="contato"
-            className="bg-[url(/portfolio-Victor-Veleda/assets/bg-stars.svg)] 
-            js-show-on-scroll transition-opacity invisible mt-28">
+            className="bg-[url(/portfolio-Victor-Veleda/assets/bg-stars.svg)] mt-28">
 
-            {/* Blur */}
-            <div
-                data-testid="blurContactSection-test"
-                className="absolute left-0 bottom-0 h-[180px]
+            <div className="js-show-on-scroll transition-opacity invisible ">
+                {/* Blur */}
+                <div
+                    data-testid="blurContactSection-test"
+                    className="absolute left-0 bottom-0 h-[180px]
                 w-[240px] -translate-y-1/2 translate-x-1/2 rounded-full bg-purble-800
                 opacity-70 blur-full mx-0 overflow-hidden"
-            />
+                />
 
-            <div className="mx-auto items-center text-center flex flex-col justify-center mb-36">
-                <h2 className="font-alt text-3xl md:text-6xl text-green-700 mb-12">Contato</h2>
-                <div className="ml-4 mr-4 border-2 border-green-700 bg-gray-500 bg-opacity-20 
+                <div className="mx-auto items-center text-center flex flex-col justify-center mb-36">
+                    <h2 className="font-alt text-3xl md:text-6xl text-green-700 mb-12">Contato</h2>
+                    <div className="ml-4 mr-4 border-2 border-green-700 bg-gray-500 bg-opacity-20 
                 p-6 shadow-gradient-green-blue shadow-green-200 rounded-xl max-w-screen-md mx-auto">
 
-                    <p title="introduction-contact" className="w-auto m-2 font-alt text-turquoise-100 text-lgPlus">
-                        Curtindo a minha jornada no mundo do desenvolvimento front-end?
-                    </p>
-                    <div
-                        className="inset-x-0 p-0 h-1 rounded-xl mb-6 w-auto mx-auto animate-underlineAnimationGradient 
+                        <p title="introduction-contact" className="w-auto m-2 font-alt text-turquoise-100 text-lgPlus">
+                            Curtindo a minha jornada no mundo do desenvolvimento front-end?
+                        </p>
+                        <div
+                            className="inset-x-0 p-0 h-1 rounded-xl mb-6 w-auto mx-auto animate-underlineAnimationGradient 
                 background-animate bg-gradient-to-r via-green-700 from-turquoise-100"
-                    />
-                    <ContactDetailsComponent />
+                        />
+                        <ContactDetailsComponent />
+                    </div>
                 </div>
             </div>
         </section>
