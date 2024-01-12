2e1a2697e8ca21e4fc91d997e89039e250f1649e
vitaolv <vlveleda@gmail.com>
2024-01-12 05:16:18 -0300

    Ajeitando Card.

--- src/app/components/CompetencyCardComponent.tsx
+++ src/app/components/CompetencyCardComponent.tsx
@@ -9,7 +9,7 @@ export function CompetencyCardComponent() {
                     <p className="font-alt text-xl mt-6 mb-3 md:h-16 w-auto sm:w-96 md:w-96 h-auto text-center">
                         {category.title}
                     </p>
-                    <div data-testid="cardCompetencies" className="border-green-700 border-2 shadow-gradient-green-blue shadow-green-700 p-10 rounded-lg md:w-96 w-auto text-center md:text-left">
+                    <div data-testid="cardCompetencies" className="border-green-700 bg-gray-500 bg-opacity-20 border-2 shadow-gradient-green-blue shadow-green-700 p-10 rounded-lg md:w-96 w-auto text-center md:text-left">
                         <ul className="flex flex-wrap justify-center gap-4">
                             {category.items.map((item) => (
                                 <li key={item.id} className="flex flex-row w-auto md:w-56 mx-auto items-center gap-4">
