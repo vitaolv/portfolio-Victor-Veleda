6ce212b3740a7897e63aef9379f0e0f2e6a2d7db
vitaolv <vlveleda@gmail.com>
2024-01-12 22:41:28 -0300

    Correção da altura de Stripes.

--- src/app/components/Stripes.tsx
+++ src/app/components/Stripes.tsx
@@ -19,7 +19,7 @@ export function StripesProjectSection() {
     return (
         <>
             {windowWidth > 1023 &&
-                <div data-testid='stripes-in-section-projects' className="absolute left-1/2 top-0 w-3 bg-stripes h-[3840px]" />
+                <div data-testid='stripes-in-section-projects' className="absolute left-1/2 top-0 w-3 bg-stripes h-[2840px]" />
             }
         </>
     )
