23790f3879408f98e5f52e964da939378acf014a
vitaolv <vlveleda@gmail.com>
2024-01-25 20:45:36 -0300

    Organizando o código.

--- src/app/utils/sections/sections.tsx
+++ src/app/utils/sections/sections.tsx
@@ -1,11 +1,12 @@
-export const sections: { key: string, value: string }[] = [{
-    key: "Home", value: "home"
-}, {
-    key: "Projetos", value: "projetos"
-}, {
-    key: "Serviços", value: "competencia"
-}, {
-    key: "Sobre mim", value: "sobre-mim"
-}, {
-    key: "Contato", value: "contato"
-}];
+export const sections: { key: string, value: string }[] = [
+    {
+        key: "Home", value: "home"
+    }, {
+        key: "Projetos", value: "projetos"
+    }, {
+        key: "Serviços", value: "competencia"
+    }, {
+        key: "Sobre mim", value: "sobre-mim"
+    }, {
+        key: "Contato", value: "contato"
+    }];
