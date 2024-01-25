37eac2c38f094c4294b5f0ebcf165de563945d3c
vitaolv <vlveleda@gmail.com>
2024-01-25 20:42:57 -0300

    Mudança de ordem de links na barra de navegação.

--- src/app/utils/sections/sections.tsx
+++ src/app/utils/sections/sections.tsx
@@ -1,11 +1,11 @@
 export const sections: { key: string, value: string }[] = [{
     key: "Home", value: "home"
 }, {
-    key: "Sobre mim", value: "sobre-mim"
-}, {
     key: "Projetos", value: "projetos"
 }, {
     key: "Serviços", value: "competencia"
+}, {
+    key: "Sobre mim", value: "sobre-mim"
 }, {
     key: "Contato", value: "contato"
 }];
