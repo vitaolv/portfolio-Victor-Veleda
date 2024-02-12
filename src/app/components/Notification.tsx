3bb280d3e6b83eb4277f78dddd1d157596226697
vitaolv <vlveleda@gmail.com>
2024-02-12 04:22:27 -0300

    Corrigindo a largura do texto.

--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -12,8 +12,8 @@ export function NotificationComponent() {
 
     return (
         <div className={`fixed inset-x-0 bottom-16 z-50 flex justify-center`}>
-            <div className={`m-6 h-fit rounded-md shadow-lg ${style}`}>
-                <p className="m-2">
+            <div className={`m-6 h-fit rounded-md shadow-lg max-w-md mx-auto ${style}`}>
+                <p className="m-2 text-center">
                     {notification.textNotification}
                 </p>
             </div>
